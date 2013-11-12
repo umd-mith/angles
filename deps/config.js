0ff97ffc53f7f4100bb1cd69b9915aeaf76a8b95
James Smith <jgsmith@gmail.com>
2013-11-12 15:20:50 -0500

    move sax.js to a submodule

--- deps/config.js
+++ deps/config.js
@@ -1,4 +1,4 @@
 var config = {
-			      'client_id': '{PUT YOUR ID HERE}.apps.googleusercontent.com',
-			      'scope': ['https://www.googleapis.com/auth/drive',"https://www.googleapis.com/auth/drive.file"]
-			    };
+  'client_id': '{PUT YOUR ID HERE}.apps.googleusercontent.com',
+  'scope': ['https://www.googleapis.com/auth/drive',"https://www.googleapis.com/auth/drive.file"]
+};
